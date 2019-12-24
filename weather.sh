b4c101d758d906373336bbe2a553fdff0825239d
ixjx <vissky1@163.com>
2019-12-24 17:32:03 +0800

    back v1

--- weather.sh
+++ weather.sh
@@ -5,6 +5,7 @@ set -eux
 CITY=Chongqing
 LANGUAGE="zh-CN"
 UA="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
+UNIT=m
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
