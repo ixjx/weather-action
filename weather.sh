077581485a0218f00e277e6195390c7aa7f925e0
ixjx <vissky1@163.com>
2019-12-24 17:06:00 +0800

    reroll

--- weather.sh
+++ weather.sh
@@ -7,10 +7,8 @@ LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 
-#curl \
-#  -H "Accept-Language: $LANGUAGE" \
-#  -H "User-Agent: $UA" \
-#  -o result.html \
-#  wttr.in/$CITY?format=4\&$UNIT
-
-wget wttr.in/$CITY.png
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  wttr.in/$CITY?format=4\&$UNIT
