95ffa8c9fd218592b020e4d9f5255072b7b047eb
ixjx <vissky1@163.com>
2019-12-24 13:51:05 +0800

    html png

--- weather.sh
+++ weather.sh
@@ -7,4 +7,8 @@ LANGUAGE="zh-CN,zh;q=0.9"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 
-wget wttr.in/$CITY.png
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o result.html \
+  wttr.in/$CITY
