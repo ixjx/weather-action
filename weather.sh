66f44dcce9f536121c1ec77c9f000923db7c7381
ixjx <vissky1@163.com>
2019-12-24 17:14:08 +0800

    to v2

--- weather.sh
+++ weather.sh
@@ -4,11 +4,10 @@ set -eux
 
 CITY=Chongqing
 LANGUAGE="zh-CN"
-UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  v2.wttr.in/$CITY
