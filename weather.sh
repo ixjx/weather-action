da25f8544dbdf89e61a30d71d7b9d69371df41ec
ixjx <vissky1@163.com>
2019-12-24 13:54:56 +0800

    html

--- weather.sh
+++ weather.sh
@@ -3,7 +3,7 @@
 set -eux
 
 CITY=Chongqing
-LANGUAGE="zh-CN,zh;q=0.9"
+LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
 
